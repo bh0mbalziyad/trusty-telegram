26b84f09933c87d49cb162e0985a021b27a41101
bh0mbalziyad <zeebhombal@gmail.com>
2020-08-15 11:32:17 +0530

    Replace TextButton with FlatButton

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -72,13 +72,13 @@ class CartItem extends StatelessWidget {
               title: Text('Are you sure?'),
               content: Text('Do you want to remove the item from the cart?'),
               actions: [
-                TextButton(
+                FlatButton(
                   child: Text('No'),
                   onPressed: () {
                     Navigator.of(ctx).pop(false);
                   },
                 ),
-                TextButton(
+                FlatButton(
                   child: Text('Yes'),
                   onPressed: () {
                     Navigator.of(ctx).pop(true);
